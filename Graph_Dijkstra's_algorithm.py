276c8c5d8c3be4e908cfcfa229361da8a6c697e2
Yuan Cheng <79687050+YuanChengRua@users.noreply.github.com>
2022-01-19 00:10:46 -0500

    build basic setting of dijkstra algorithm

--- Graph_Dijkstra's_algorithm.py
+++ Graph_Dijkstra's_algorithm.py
@@ -10,3 +10,40 @@ Dijkstra's step:
     4. Suppose the distance between v2 to v3 is the smallest, we move from v2 to v3 so we can update the distance from v1 to v3's neighbors v4 v5 v6...
     5. we need to ensure that we have gone through all the v then we can end the algorithm.
 '''
+
+
+'''
+Implementation
+
+We need 3 hashmaps: graph, cost, parents
+'''
+
+graph = {}  # This is a big hashmap
+graph['start'] = {}
+graph['start']['a'] = 6  # graph['start'] is also a hashmap
+graph['start']['b'] = 2
+
+graph['a'] = {}
+graph['a']['fin'] = 1
+
+graph['b'] = {}
+graph['b']['a'] = 3
+graph['b']['fin'] = 5
+
+graph['fin'] = {}
+
+# define infinity
+infinity = float("inf")
+
+# cost hash means the distance from the start to all other vertices
+cost = {}
+cost['a'] = 6
+cost['b'] = 2
+cost['fin'] = infinity
+
+# parents hash means the parents of all the vertices, the vertices which have not been reached noted as None
+parents = {}
+parents['a'] = 'start'
+parents['b'] = 'start'
+parents['fin'] = None
+
